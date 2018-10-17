40f36fed7be8371c5cf1fc2de145732f6db2fa80
s7130457 <s7130457@gamil.com>
2018-10-17 11:35:58 +0800

    refactory code

--- server/error.js
+++ server/error.js
@@ -7,21 +7,18 @@ module.exports = () => {
         try {
             await next();
         } catch (err) {
-            if(process.env.NODE_ENV !== 'production') {
-                msg = err.message;
-                result = resp.internalError(msg);
-                ctx.body = result;
-                ctx.status = result.statusCode;
-            } 
-            else if (process.env.NODE_ENV === 'production') {
+            if(process.env.NODE_ENV === 'production') {
                 result = resp.internalError('Internal Server Error');
                 ctx.body = result;
                 ctx.status = result.statusCode;
             } 
             else {
-                console.log('ELSE');
-                console.log('process.env.NODE_ENV is ' + process.env.NODE_ENV );
-            }
+                msg = err.message;
+                result = resp.internalError(msg);
+                ctx.body = result;
+                ctx.status = result.statusCode;
+            } 
+            console.log(err);
         }
     };
 };
